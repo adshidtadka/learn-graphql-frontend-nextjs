8800bb4fac3c9ca07ab3e2590fa1fdcd517aff26
adshidtadka <nextage900fx@gmail.com>
2023-01-21 14:45:32 +0900

    add todo

--- components/Todo/TodoInput.js
+++ components/Todo/TodoInput.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+
+const ADD_TODO = gql`
+  mutation ($todo: String!, $isPublic: Boolean!) {
+    insert_todos(objects: { title: $todo, is_public: $isPublic }) {
+      affected_rows
+      returning {
+        id
+        title
+        created_at
+        is_completed
+      }
+    }
+  }
+`;
 
 const TodoInput = ({ isPublic = false }) => {
+  let input;
+
+  const [todoInput, setTodoInput] = useState("");
+
+  const [addTodo] = useMutation(ADD_TODO);
   return (
     <form
       className="formInput"
-      onSubmit={e => {
+      onSubmit={(e) => {
         e.preventDefault();
+        addTodo({ variables: { todo: todoInput, isPublic } });
       }}
     >
-      <input className="input" placeholder="What needs to be done?" />
+      <input
+        className="input"
+        placeholder="What needs to be done?"
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+        ref={(n) => (input = n)}
+      />
       <i className="inputMarker fa fa-angle-right" />
     </form>
   );
