7c150a1d88f3f0578e843a5c2aebfba5cbba4755
adshidtadka <nextage900fx@gmail.com>
2023-01-21 14:50:52 +0900

    toggle todo

--- components/Todo/TodoItem.js
+++ components/Todo/TodoItem.js
@@ -1,12 +1,49 @@
 import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import { GET_MY_TODOS } from "./TodoPrivateList";
+
+const TOGGLE_TODO = gql`
+  mutation toggleTodo($id: Int!, $isCompleted: Boolean!) {
+    update_todos(
+      where: { id: { _eq: $id } }
+      _set: { is_completed: $isCompleted }
+    ) {
+      affected_rows
+    }
+  }
+`;
 
 const TodoItem = ({ index, todo }) => {
-  const removeTodo = e => {
+  const removeTodo = (e) => {
     e.preventDefault();
     e.stopPropagation();
   };
 
-  const toggleTodo = () => {};
+  const [toggleTodoMutation] = useMutation(TOGGLE_TODO);
+  const toggleTodo = () => {
+    toggleTodoMutation({
+      variables: { id: todo.id, isCompleted: !todo.is_completed },
+      optimisticResponse: {
+        update_todos: {
+          affected_rows: 1,
+        },
+      },
+      update: (cache) => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.map((t) => {
+          if (t.id === todo.id) {
+            return { ...t, is_completed: !t.is_completed };
+          } else {
+            return t;
+          }
+        });
+        cache.writeQuery({
+          query: GET_MY_TODOS,
+          data: { todos: newTodos },
+        });
+      },
+    });
+  };
 
   return (
     <li>
