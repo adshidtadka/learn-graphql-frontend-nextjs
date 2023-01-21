0db736465d7b268f1417c882a4ec4a4732374d7c
adshidtadka <nextage900fx@gmail.com>
2023-01-21 14:47:19 +0900

    update cache

--- components/Todo/TodoInput.js
+++ components/Todo/TodoInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { gql, useMutation } from "@apollo/client";
+import { GET_MY_TODOS } from "./TodoPrivateList";
 
 const ADD_TODO = gql`
   mutation ($todo: String!, $isPublic: Boolean!) {
@@ -20,7 +21,31 @@ const TodoInput = ({ isPublic = false }) => {
 
   const [todoInput, setTodoInput] = useState("");
 
-  const [addTodo] = useMutation(ADD_TODO);
+  const updateCache = (cache, { data }) => {
+    // If this is for the public feed, do nothing
+    if (isPublic) {
+      return null;
+    }
+    // Fetch the todos from the cache
+    const existingTodos = cache.readQuery({
+      query: GET_MY_TODOS,
+    });
+    // Add the new todo to the cache
+    const newTodo = data.insert_todos.returning[0];
+    cache.writeQuery({
+      query: GET_MY_TODOS,
+      data: { todos: [newTodo, ...existingTodos.todos] },
+    });
+  };
+
+  const resetInput = () => {
+    setTodoInput("");
+  };
+
+  const [addTodo] = useMutation(ADD_TODO, {
+    update: updateCache,
+    onCompleted: resetInput,
+  });
   return (
     <form
       className="formInput"
